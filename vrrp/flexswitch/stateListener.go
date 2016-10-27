7ac90202f5310f32d2ffd6cc7027c33d3f2ebd53
jgheewala <jgheewala@snaproute.com>
2016-10-26 19:49:06 -0700

    vrrp v6 state object

--- vrrp/flexswitch/stateListener.go
+++ vrrp/flexswitch/stateListener.go
@@ -31,7 +31,7 @@ import (
 	"vrrpd"
 )
 
-func (h *VrrpHandler) convertVrrpIntfEntryToThriftEntry(state vrrpd.VrrpV4IntfState) *vrrpd.VrrpV4IntfState {
+func (h *ConfigHandler) convertVrrpV4IntfEntryToThriftEntry(state vrrpd.VrrpV4IntfState) *vrrpd.VrrpV4IntfState {
 	entry := vrrpd.NewVrrpV4IntfState()
 	entry.VirtualRouterMACAddress = state.VirtualRouterMACAddress
 	entry.PreemptMode = bool(state.PreemptMode)
@@ -47,20 +47,88 @@ func (h *VrrpHandler) convertVrrpIntfEntryToThriftEntry(state vrrpd.VrrpV4IntfSt
 	return entry
 }
 
-func (h *VrrpHandler) GetBulkVrrpV4IntfState(fromIndex vrrpd.Int, count vrrpd.Int) (*vrrpd.VrrpV4IntfStateGetInfo, error) {
-	nextIdx, currCount, vrrpIntfStateEntries := h.server.VrrpGetBulkVrrpIntfStates(int(fromIndex), int(count))
-	if vrrpIntfStateEntries == nil {
-		return nil, errors.New("Interface Slice is not initialized")
-	}
-	vrrpEntryResponse := make([]*vrrpd.VrrpIntfState, len(vrrpIntfStateEntries))
-	for idx, item := range vrrpIntfStateEntries {
-		vrrpEntryResponse[idx] = h.convertVrrpV4IntfEntryToThriftEntry(item)
-	}
-	intfEntryBulk := vrrpd.NewVrrpV4IntfStateGetInfo()
-	intfEntryBulk.VrrpIntfStateList = vrrpEntryResponse
-	intfEntryBulk.StartIdx = fromIndex
-	intfEntryBulk.EndIdx = vrrpd.Int(nextIdx)
-	intfEntryBulk.Count = vrrpd.Int(currCount)
-	intfEntryBulk.More = (nextIdx != 0)
-	return intfEntryBulk, nil
+func (h *ConfigHandler) convertVrrpV6IntfEntryToThriftEntry(state vrrpd.VrrpV6IntfState) *vrrpd.VrrpV6IntfState {
+	entry := vrrpd.NewVrrpV4IntfState()
+	entry.VirtualRouterMACAddress = state.VirtualRouterMACAddress
+	entry.PreemptMode = bool(state.PreemptMode)
+	entry.AdvertisementInterval = int32(state.AdvertisementInterval)
+	entry.VRID = int32(state.VRID)
+	entry.Priority = int32(state.Priority)
+	entry.SkewTime = int32(state.SkewTime)
+	entry.VirtualIPv4Addr = state.VirtualIPv4Addr
+	//entry.IfIndex = int32(state.IfIndex)
+	entry.MasterDownTimer = int32(state.MasterDownTimer)
+	entry.IntfIpAddr = state.IntfIpAddr
+	entry.VrrpState = state.VrrpState
+	return entry
+}
+
+func (h *ConfigHandler) GetBulkVrrpV4IntfState(fromIndex vrrpd.Int, count vrrpd.Int) (*vrrpd.VrrpV4IntfStateGetInfo, error) {
+	/*
+		nextIdx, currCount, vrrpIntfStateEntries := h.server.VrrpGetBulkVrrpIntfStates(int(fromIndex), int(count))
+		if vrrpIntfStateEntries == nil {
+			return nil, errors.New("Interface Slice is not initialized")
+		}
+		vrrpEntryResponse := make([]*vrrpd.VrrpIntfState, len(vrrpIntfStateEntries))
+		for idx, item := range vrrpIntfStateEntries {
+			vrrpEntryResponse[idx] = h.convertVrrpV4IntfEntryToThriftEntry(item)
+		}
+		intfEntryBulk := vrrpd.NewVrrpV4IntfStateGetInfo()
+		intfEntryBulk.VrrpIntfStateList = vrrpEntryResponse
+		intfEntryBulk.StartIdx = fromIndex
+		intfEntryBulk.EndIdx = vrrpd.Int(nextIdx)
+		intfEntryBulk.Count = vrrpd.Int(currCount)
+		intfEntryBulk.More = (nextIdx != 0)
+		return intfEntryBulk, nil
+	*/
+	return nil, nil
+}
+
+func (h *ConfigHandler) GetVrrpV4IntfState(ifIndex int32, vrId int32) (*vrrpd.VrrpV4IntfState, error) {
+	/*
+		response := vrrpd.NewVrrpV4IntfState()
+		key := strconv.Itoa(int(ifIndex)) + "_" + strconv.Itoa(int(vrId))
+		rv := h.server.VrrpPopulateIntfState(key, response)
+		if !rv {
+			return nil, errors.New(VRRP_SVR_NO_ENTRY + strconv.Itoa(int(ifIndex)) +
+				" and Virtual Router Id:" + strconv.Itoa(int(vrId)))
+		}
+		return response, nil
+	*/
+	return nil, nil
+}
+
+func (h *ConfigHandler) GetBulkVrrpV6IntfState(fromIndex vrrpd.Int, count vrrpd.Int) (*vrrpd.VrrpV6IntfStateGetInfo, error) {
+	/*
+		nextIdx, currCount, vrrpIntfStateEntries := h.server.VrrpGetBulkVrrpIntfStates(int(fromIndex), int(count))
+		if vrrpIntfStateEntries == nil {
+			return nil, errors.New("Interface Slice is not initialized")
+		}
+		vrrpEntryResponse := make([]*vrrpd.VrrpIntfState, len(vrrpIntfStateEntries))
+		for idx, item := range vrrpIntfStateEntries {
+			vrrpEntryResponse[idx] = h.convertVrrpV4IntfEntryToThriftEntry(item)
+		}
+		intfEntryBulk := vrrpd.NewVrrpV4IntfStateGetInfo()
+		intfEntryBulk.VrrpIntfStateList = vrrpEntryResponse
+		intfEntryBulk.StartIdx = fromIndex
+		intfEntryBulk.EndIdx = vrrpd.Int(nextIdx)
+		intfEntryBulk.Count = vrrpd.Int(currCount)
+		intfEntryBulk.More = (nextIdx != 0)
+		return intfEntryBulk, nil
+	*/
+	return nil, nil
+}
+
+func (h *ConfigHandler) GetVrrpV6IntfState(ifIndex int32, vrId int32) (*vrrpd.VrrpV6IntfState, error) {
+	/*
+		response := vrrpd.NewVrrpV4IntfState()
+		key := strconv.Itoa(int(ifIndex)) + "_" + strconv.Itoa(int(vrId))
+		rv := h.server.VrrpPopulateIntfState(key, response)
+		if !rv {
+			return nil, errors.New(VRRP_SVR_NO_ENTRY + strconv.Itoa(int(ifIndex)) +
+				" and Virtual Router Id:" + strconv.Itoa(int(vrId)))
+		}
+		return response, nil
+	*/
+	return nil, nil
 }
